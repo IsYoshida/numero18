d62b7809706ca1b65bce1c1e7a149f4ad93dafbc
IsYoshida <80777620+IsYoshida@users.noreply.github.com>
2021-08-13 17:05:35 -0300

    Add files via upload

---
+++ src/js/helproleta.js
@@ -0,0 +1,11 @@
+const helproleta = `Roleta russa consiste em escolher um membro aleatório do grupo e de acordo com a dificuldade, o membro escolhido sofrerá as consequências.
+É bem fácil de jogar, basta apenas dar o comando que começa em automático. Olhe abaixo as consequências de cada dificuldade.
+
+roletarussapac - não faz nada, apenas diversão
+roletarussapac - Elimina o membro escolhido
+roletarussamed - Elimina e bloqueia o membro escolhido
+roletarussahard - Elimina e bloqueia o membro escolhido
+
+Obs: os efeitos são reversíveis, para desbloquear o membro basta apenas dar unblock (número sem espaço, traço ou +)`
+
+exports.helproleta = helproleta
